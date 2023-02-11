5870f4f126b85759486f6832b33a98a76165eab1
jinbeizame007 <jinbeizame007@gmail.com>
2023-02-11 19:18:23 +0900

    [add] test for CircleObstacle

--- tests/obstacle.rs
+++ tests/obstacle.rs
@@ -6,11 +6,23 @@ fn test_rect_obstacle() {
     let center: [f32; 2] = [0.0, 0.0];
     let size: [f32; 2] = [2.0, 2.0];
     let rect_obstacle = pp::env::RectObstacle{center, size};
-    assert_eq!(rect_obstacle.center[0], 0.0);
 
     let position: [f32; 2] = [0.5,0.5];
     assert!(rect_obstacle.is_inside(&position));
 
     let position: [f32; 2] = [2.1,2.1];
     assert!(!rect_obstacle.is_inside(&position));
+}
+
+#[test]
+fn test_circle_obstacle() {
+    let center: [f32; 2] = [0.0, 0.0];
+    let radius = 2.0;
+    let circle_obstacle = pp::env::CircleObstacle{center, radius};
+    
+    let position: [f32; 2] = [1.2, 1.2];
+    assert!(circle_obstacle.is_inside(&position));
+    
+    let position: [f32; 2] = [1.8, 1.8];
+    assert!(!circle_obstacle.is_inside(&position));
 }
