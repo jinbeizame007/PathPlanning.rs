59498328543355b1c3d966ed46bd08472c4365ed
jinbeizame007 <jinbeizame007@gmail.com>
2023-02-25 13:35:48 +0900

    [add] RRT constructor

--- src/planner.rs
+++ src/planner.rs
@@ -1,3 +1,5 @@
 mod node;
+mod rrt;
 pub use crate::planner::node::Node;
 pub use crate::planner::node::calc_distance;
+pub use crate::planner::rrt::RRT;
---
+++ src/planner/rrt.rs
@@ -0,0 +1,32 @@
+pub use crate::planner::node::Node;
+
+pub struct RRT<const D: usize> {
+    pub start_node: Node<D>,
+    pub goal_node: Node<D>,
+    pub low: [f32; D],
+    pub high: [f32; D],
+    pub nodes: Vec<Node<D>>,
+    goal_sample_rate: f32,
+    step_size: f32,
+    max_iter: i32,
+}
+
+impl<const D: usize> RRT<D> {
+    pub fn new(
+        start: [f32; D],
+        goal: [f32; D],
+        low: [f32; D],
+        high: [f32; D]
+    ) -> Self {
+        RRT{
+            start_node: Node::new(start),
+            goal_node: Node::new(goal),
+            low: low,
+            high: high,
+            nodes: vec![Node::new(start)],
+            goal_sample_rate: 0.7,
+            step_size: 0.5,
+            max_iter: 1000,
+        }
+    }
+}
