c444844c2b3440b940a93b7bb091b9b384bb256b
jinbeizame007 <jinbeizame007@gmail.com>
2023-02-25 14:53:23 +0900

    [add] calc_difference() for Node

--- src/planner.rs
+++ src/planner.rs
@@ -2,4 +2,5 @@ mod node;
 mod rrt;
 pub use crate::planner::node::Node;
 pub use crate::planner::node::calc_distance;
+pub use crate::planner::node::calc_difference;
 pub use crate::planner::rrt::RRT;
--- src/planner/node.rs
+++ src/planner/node.rs
@@ -16,3 +16,11 @@ pub fn calc_distance<const D: usize>(node1: &Node<D>, node2: &Node<D>) -> f32 {
     }
     return distance.powf(0.5);
 }
+
+pub fn calc_difference<const D: usize>(node1: &Node<D>, node2: &Node<D>) -> [f32; D] {
+    let mut difference: [f32; D] = [0.0; D];
+    for i in 0..D {
+        difference[i] = node2.position[i] - node1.position[i];
+    }
+    return difference;
+}
