0b0d8355aec5ff50a258333a5c90cd80a266caca
jinbeizame007 <jinbeizame007@gmail.com>
2023-02-26 17:17:44 +0900

    [add] draw_env()

--- src/plot/plot2d.rs
+++ src/plot/plot2d.rs
@@ -1,23 +1,13 @@
 use crate::env::Env;
 use crate::env::Obstacle;
 use plotters::prelude::*;
+use plotters::coord::types::RangedCoordf32;
 
 const MARGIN: i32 = 20;
 const X_LABEL_AREA_SIZE: i32 = 30;
 const Y_LABEL_AREA_SIZE: i32 = 30;
 
-pub fn plot_env(env: &Env<2>) -> Result<(), Box<dyn std::error::Error>> {
-    let root = BitMapBackend::new("env.png", (1000, 600)).into_drawing_area();
-    root.fill(&WHITE)?;
-
-    let mut chart = ChartBuilder::on(&root)
-        .margin(MARGIN)
-        .x_label_area_size(X_LABEL_AREA_SIZE)
-        .y_label_area_size(Y_LABEL_AREA_SIZE)
-        .build_cartesian_2d(env.low[0]..env.high[0], env.low[1]..env.high[1])
-        .unwrap();
-    chart.configure_mesh().draw().unwrap();
-
+pub fn draw_env(chart: &mut ChartContext<BitMapBackend, Cartesian2d<RangedCoordf32, RangedCoordf32>>, env: &Env<2>) {
     for i in 0..env.obstacles.len() {
         let obs = &env.obstacles[i];
         match obs {
@@ -40,6 +30,21 @@ pub fn plot_env(env: &Env<2>) -> Result<(), Box<dyn std::error::Error>> {
             }
         };
     }
+}
+
+pub fn plot_env(env: &Env<2>) -> Result<(), Box<dyn std::error::Error>> {
+    let root = BitMapBackend::new("env.png", (1000, 600)).into_drawing_area();
+    root.fill(&WHITE)?;
+
+    let mut chart = ChartBuilder::on(&root)
+        .margin(MARGIN)
+        .x_label_area_size(X_LABEL_AREA_SIZE)
+        .y_label_area_size(Y_LABEL_AREA_SIZE)
+        .build_cartesian_2d(env.low[0]..env.high[0], env.low[1]..env.high[1])
+        .unwrap();
+    chart.configure_mesh().draw().unwrap();
+
+    draw_env(&mut chart, &env);
 
     root.present()?;
     Ok(())
