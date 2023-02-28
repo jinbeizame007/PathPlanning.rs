2caeacd8e34303b43ec1da46542bb3f41abde718
jinbeizame007 <jinbeizame007@gmail.com>
2023-02-28 22:19:38 +0900

    [add] apply is_approved to example of plot_2d_path

--- examples/plot_2d_path.rs
+++ examples/plot_2d_path.rs
@@ -5,15 +5,21 @@ fn main() {
 
     let start: [f32; 2] = [1.0, 1.0];
     let goal: [f32; 2] = [48.0, 25.0];
+    let low: [f32; 2] = env.low.clone();
+    let high: [f32; 2] = env.high.clone();
+
+    let _env = env.clone();
+    let is_approved = Box::new(move |position: &[f32; 2]| !_env.is_inside_obstacle(position));
 
     let rrt = pp::planner::RRT::new(
         start,
         goal,
-        env.low.clone(),
-        env.high.clone(),
+        low,
+        high,
+        is_approved,
         0.2,
         2.0,
-        1000,
+        2000,
     );
     let path = rrt.plan();
     pp::plot::plot_path(&env, &path).unwrap();
